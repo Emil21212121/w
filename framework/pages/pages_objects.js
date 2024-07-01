f71447faa1c760c8d795c3a68f4ed8433ac8fd9c
Emil21212121 <atis21000@gmail.com>
2024-07-01 14:28:39 +0400

    12

--- framework/pages/pages_objects.js
+++ framework/pages/pages_objects.js
@@ -1,20 +1,186 @@
 
 import { expect } from '@playwright/test';
-
+import { assert } from 'console';
+// Лобби
 export function lobbyPage(page) {
     return {
         // Метод для перехода на страницу Лобби
         async goto() {
-            await page.locator('a').filter({ hasText: 'Лобби' }).click();
+            await page.getByRole('button', { name: 'Лобби' }).click();
         },
 
         // Методы для проверок на странице Лобби
-        async assertOnLobbyPage() {
+        async assertLobbyPage () {
             await expect(page).toHaveURL(/.*lobby/);
         },
 
-        async assertLobbyVisible() {
+        async assertLobbyVisible () {
             await expect(page.locator('#scrollArea').getByText('лобби')).toBeVisible();
         }
     };
 }
+
+// Гонка
+export function racePage (page) {
+    return {
+        async goto() {
+            await page.getByRole('button', { name: 'Гонка' }).click();
+        },
+
+        async assertRacePage () {
+            await expect(page).toHaveURL(/.*race/);
+        },
+
+        async assertRaceVisible () {
+            await expect(page.getByRole('cell', { name: 'Игрок' })).toBeVisible();
+            await expect(page.getByRole('cell', { name: 'Сумма ставок' })).toBeVisible();
+            await expect(page.getByRole('cell', { name: 'Приз' })).toBeVisible();
+        }
+    }
+}
+// Акции
+export function promotionsPage (page) {
+    return {
+        async goto () {
+            await page.getByRole('button', { name: 'Акции' }).click();
+        },
+
+        async assertPromotionsPage () {
+            await expect(page).toHaveURL(/.*promotions/);
+        },
+
+        async assertPromotionsVisible () {
+            await expect(page.getByText('Акции и предложенияНе пропустите возможность получить бонус в наших текущих акци')).toBeVisible();
+        }
+    }
+}
+// Турниры
+export function tournamentsPage (page) {
+    return {
+        async goto () {
+            await page.getByRole('button', { name: 'Турниры' }).click();
+        },
+
+        async assertTournamentPage () {
+            await expect(page).toHaveURL(/.*tournaments/);
+        },
+
+        async assertTournamentVisible () {
+            await expect(page.getByText('ТурнирыПоучаствуй в наших турнирах и забери свой приз')).toBeVisible();
+        }
+    }
+}
+// Вип клуб
+export function VipClubPage (page) {
+    return {
+        async goto () {
+            await page.getByRole('button', { name: 'VIP-клуб' }).click();
+        },
+
+        async assertVipClubPage () {
+            await expect(page).toHaveURL(/.*club/);
+        },
+
+        async assertVipClubVisible () {
+            await expect(page.getByText('Selector VIP CLUB', { exact: true })).toBeVisible();
+            await page.locator('#scrollArea div').filter({ hasText: 'Непревзойденный VIP' }).nth(3).click();
+            await expect(page.getByText('Уровни VIP')).toBeVisible();
+            await expect(page.getByText('Высочайшая доходность для игрокаНаши бонусы абсолютно прозрачные. Все полученные бонусы вы сможете сразу выводить или снова пускать в игру. Только чистый профит без дополнительных условий для игроков и других подводных камней.Эксклюзивные и гибкие бонусыМы ценим каждого игрока. Ваш VIP-менеджер может поздравить вас с днем рождения и подарить вам новенький MacBook или подарить крупный бонус-код к новому году. Все это и многое другое возможно в нашем VIP-клубе')).toBeVisible();
+            await expect(page.getByText('БронзаСумма ставок от 2 332.')).toBeVisible();
+            await page.getByRole('button', { name: 'Серебро' }).click();
+            await expect(page.getByText('СереброСумма ставок от 11 662')).toBeVisible();
+            await page.getByRole('button', { name: 'Золото' }).click();
+            await expect(page.getByText('ЗолотоСумма ставок от 69 972.')).toBeVisible();
+            await page.getByRole('button', { name: 'Платина' }).click();
+            await expect(page.getByText('ПлатинаСумма ставок от 174')).toBeVisible();
+            await page.getByRole('button', { name: 'Алмаз' }).click();
+            await expect(page.getByText('АлмазСумма ставок от 349 862.')).toBeVisible();
+            await page.getByRole('button', { name: 'Элит' }).click();
+            await expect(page.getByText('ЭлитСумма ставок от 699 724.')).toBeVisible();
+        }
+    }
+}
+// Промо
+export function promoPage (page) {
+    return {
+        async goto () {
+            await page.getByRole('button', { name: 'Промо' }).click();
+        },
+
+        async assertPromoPage () {
+            await expect(page).toHaveURL(/.*promo/);
+        },
+
+        async assertPromoVisible () {
+            await expect(page.getByText('Привяжи аккаунт Telegram')).toBeVisible();
+            await expect(page.getByRole('main').locator('div').filter({ hasText: 'КрутитьКрути рулетку и получай бонусы!Раз в 15' }).nth(3)).toBeVisible();
+            await expect(page.getByText('РефералыПриглашай друзей и зарабатывай 60')).toBeVisible();
+            await expect(page.getByText('Selector VIP ClubВступай в лучшую программу лояльностиVIP-клуб')).toBeVisible();
+        }
+    }
+}
+// Партнеры
+export function partnerPage (page) {
+    return {
+        async goto () {
+            await page.getByRole('button', { name: 'Партнерам' }).click();
+        },
+
+        async assertPartnerPage () {
+            await expect(page).toHaveURL(/.*partner/);
+        },
+
+        async assertPartnerVisible () {
+            await expect(page.locator('div').filter({ hasText: /^Общее$/ })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'ID партнера: #' })).toBeVisible();
+            await expect(page.getByText('Кабинет партнера')).toBeVisible();
+        },
+
+        async campaignCection () {
+            await page.getByText('Кампании').click();
+        },
+
+        async campaignCectionVisible () {
+            await expect(page.locator('div').filter({ hasText: /^Кампании$/ })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Создать' })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Обновить' })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Экспорт' })).toBeVisible();
+        },
+
+        async staticsCection () {
+            await page.getByText('Статистика').click();
+        },
+
+        async staticsCectionVisible () {
+            await expect(page.locator('div').filter({ hasText: /^Статистика$/ })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Таблицей' })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Графиком' })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Экспорт' })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Общая статистика' })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Основная ссылка' })).toBeVisible();
+        },
+
+        async playersCection () {
+            await page.getByText('Игроки').click();
+        },
+
+        async playersCectionVisible () {
+            await expect(page.locator('div').filter({ hasText: /^Игроки$/ })).toBeVisible();
+        },
+
+        async accrualLogCection () {
+            await page.getByText('Лог начислений').click();
+        },
+
+        async accrualLogCectionVisible () {
+            await expect(page.locator('div').filter({ hasText: /^Лог начислений$/ })).toBeVisible();
+        }
+    }
+}
+
+
+
+    
+    
+
+  
--- tests/ui.spec.js
+++ tests/ui.spec.js
@@ -1,6 +1,6 @@
 import { test, expect, describe } from '@playwright/test';
 import { baseURL } from '../framework/config/config';
-
+import {lobbyPage, racePage, promotionsPage, tournamentsPage, VipClubPage, partnerPage, promoPage} from '../framework/pages/pages_objects'
 describe('Тесты навигации', () => {
   let page;
 
@@ -23,98 +23,123 @@ describe('Тесты навигации', () => {
     });
 
     await page.reload();
+    await page.locator('button').first().click();
+ 
   });
 
   // Тесты
 
   test('Переход на страницу Лобби', async () => {
     // Переходим на страницу Лобби
-    await page.locator('a').filter({ hasText: 'Лобби' }).click();
-    
+    await lobbyPage(page).goto();
+
     // Обработчики
-    await expect(page).toHaveURL(/.*lobby/);
-    await expect(page.locator('#scrollArea').getByText('лобби')).toBeVisible();
+    await lobbyPage(page).assertLobbyPage();
+    await lobbyPage(page).assertLobbyVisible();
+    
   });
 
   test('Переход на страницу Гонка', async () => {
     // Переход на страницу Гонка
-    await page.locator('a', { hasText: /^Гонка$/ }).click();
+    await racePage(page).goto();
 
     // Обработчики
-    await expect(page).toHaveURL(/.*race/);
-    await expect(page.getByText('Игрок')).toBeVisible();
-    await expect(page.getByText('Сумма ставок')).toBeVisible();
-    await expect(page.getByText('Приз', { exact: true })).toBeVisible();
+    await racePage(page).assertRacePage();
+    await racePage(page).assertRaceVisible();
+
   });
 
   test('Переход на страницу Промоакций', async () => {
 
     // Переходим на страницу Промоакций
-    await page.locator('a').filter({ hasText: 'Акции' }).click();
+    await promotionsPage(page).goto();
+
+    // Обработчики
+    await promotionsPage(page).assertPromotionsPage();
+    await promotionsPage(page).assertPromotionsVisible();
+    
+    
+    
+  });
+
+  test('Переход на страницу Турниры', async () => {
+
+    // Переход на страницу Турниры
+    await tournamentsPage(page).goto();
     
     // Обработчики
-    await expect(page).toHaveURL(/.*promotions/);
-    await expect(page.getByText('Акции и предложенияНе пропустите возможность получить бонус в наших текущих акци')).toBeVisible();
+    await tournamentsPage(page).assertTournamentPage();
+    await tournamentsPage(page).assertTournamentVisible();
   });
 
   test('Переход на страницу ВИП клуба', async () => {
 
     // Переход на страницу ВИП клуба
-    await page.locator('a').filter({ hasText: 'VIP-клуб' }).click();
+    await VipClubPage(page).goto()
 
     // Обработчики
-    await expect(page.getByRole('button', { name: 'Бронза' })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Серебро' })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Золото' })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Платина' })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Алмаз' })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Элит' })).toBeVisible();
+    await VipClubPage(page).assertVipClubPage()
+    await VipClubPage(page).assertVipClubVisible()
   });
 
-  test('Переход на Релоад', async () => {
-    await page.getByRole('button', { name: 'Релоад' }).click();
-
-    await expect(page.locator('div').filter({ hasText: /^Релоад$/ })).toBeVisible();
-  });
-
-  test('Переход на Рейкбэк', async () => {
-    await page.getByRole('button', { name: 'Рейкбэк' }).click();
-
-    await expect(page.locator('div').filter({ hasText: /^Рейкбэк$/ })).toBeVisible();
-  });
-
-  test('Переход на Буст', async () => {
-    await page.getByRole('button', { name: 'Буст' }).click();
-
-    await expect(page.locator('div').filter({ hasText: /^Буст$/ })).toBeVisible();
-    await page.locator('._modal-close_9g9mb_3581').click();
-  });
+  test ('Переходим на страницу Промо', async () => {
+    await promoPage(page).goto();
+    await promoPage(page).assertPromoPage();
+    await promoPage(page).assertPromoVisible();
+  })
 
   test('Переход на страницу Партнерам', async () => {
 
     // Переходим на страницу Партнерам
-    await page.locator('a').filter({ hasText: 'Партнерам' }).click();
-
-    await expect(page.locator('div').filter({ hasText: /^Общее$/ })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'ID партнера: #' })).toBeVisible();
-    await expect(page.getByText('Кабинет партнера')).toBeVisible();
-  });
-
-  test('Переход в раздел Компании', async () => {
+    await partnerPage(page).goto();
+    await partnerPage(page).assertPartnerPage();
+    await partnerPage(page).assertPartnerVisible();
+   
+    // Обработчики
+    await partnerPage(page).assertPartnerPage();
+    await partnerPage(page).assertPartnerVisible();
 
     // Переходим в раздел Кампании
-    await page.getByText('Кампании').click();
-    
-    // Обработчики
-    await expect(page).toHaveURL(/.*partner/);
-    await expect(page.locator('div').filter({ hasText: /^Кампании$/ })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Создать' })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Обновить' })).toBeVisible();
-    await expect(page.getByRole('button', { name: 'Экспорт' })).toBeVisible();
-  });
+    await partnerPage(page).campaignCection();
+    await partnerPage(page).campaignCectionVisible();
 
+    // Переходим в раздел Статистика
+    await partnerPage(page).staticsCection();
+    await partnerPage(page).staticsCectionVisible();
+
+    // Переходим в раздел Игроки
+    await partnerPage(page).playersCection();
+    await partnerPage(page).playersCectionVisible();
+
+   // Переходим в раздел Лог начислений 
+   await partnerPage(page).accrualLogCection();
+   await partnerPage(page).accrualLogCectionVisible();
+
+  });
+  
   test.afterAll(async () => {
     // Закрыть страницу
     await page.close();
   });
 });
+
+
+/*
+test('Переход на Релоад', async () => {
+  await page.getByRole('button', { name: 'Релоад' }).click();
+
+  await expect(page.locator('div').filter({ hasText: /^Релоад$/ })).toBeVisible();
+});
+
+test('Переход на Рейкбэк', async () => {
+  await page.getByRole('button', { name: 'Рейкбэк' }).click();
+
+  await expect(page.locator('div').filter({ hasText: /^Рейкбэк$/ })).toBeVisible();
+});
+
+test('Переход на Буст', async () => {
+  await page.getByRole('button', { name: 'Буст' }).click();
+
+  await expect(page.locator('div').filter({ hasText: /^Буст$/ })).toBeVisible();
+  await page.locator('._modal-close_9g9mb_3581').click();
+});*/
